7646823b7e6b9379116876d24d3687291eff5b0e
nitish@gaga <nitishgoku01@gmail.com>
2022-05-10 17:26:27 +0530

    comment page styling

--- cc-frontend/src/components/Comments.js
+++ cc-frontend/src/components/Comments.js
@@ -10,44 +10,48 @@ function Comments() {
         }
       />
       <div className="w-full p-4">
-        <div className="bg-gray-100 w-full border rounded-lg">
+        <div className="px-4 py-2 bg-gray-100 w-full border rounded-lg">
           <div className='px-4 py-2'>
           Comments
           </div>
-          {/* <div class="flex h-screen bg-gray-800 justify-center items-center"> */}
-            <div class="w-full  p-2 pt-2 rounded ">
-              <div class="flex ml-3">
-                <div class="mr-3">
+            <div className="w-full mb-3 rounded ">
+              <div className="flex ml-3">
+                <div className="mr-3">
                   <img
                     src="http://picsum.photos/50"
                     alt=""
-                    class="rounded-full"
+                    className="rounded-full"
                   />
                 </div>
                 <div className='flex items-center'>
-                  <h1 class="mb-1 font-semibold">Itay Buyoy</h1>
+                  <h1 className="mb-1 font-semibold">Itay Buyoy</h1>
                 </div>
               </div>
 
-              <div class="mt-3 p-3 w-full">
+              <div className="mt-3 p-3 w-full">
                 <textarea
                   rows="3"
-                  class="border p-2 rounded w-full"
+                  className="border p-2 rounded w-full"
                   placeholder="Write something..."
                 ></textarea>
               </div>
 
-              <div class="flex justify-between mx-3">
+              <div className="flex justify-between mx-3">
                 <div>
-                  <button class="px-4 py-1 bg-gray-800 text-white rounded font-light hover:bg-gray-700">
+                  <button className="px-4 py-1 bg-gray-800 text-white rounded font-light hover:bg-gray-700">
                     Submit
                   </button>
                 </div>
               </div>
             </div>
+            <div className=' h-[1px] bg-gray-400'/>
+            <div className='w-full mt-6 pt-2 px-4 py-2 rounded'>
+
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio velit vero doloremque soluta dolor sed totam cum culpa neque nesciunt odio, voluptatum provident repudiandae nobis maxime possimus aliquam atque cupiditate.
+            </div>
           </div>
+          
         </div>
-      {/* </div> */}
     </div>
   )
 }
